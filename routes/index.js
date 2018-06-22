ef2afd05b9b9cd62bfa8f78b1c2ba3484352cb49
Connor Murphy <dev.web@engineer.com>
2018-06-22 01:29:19 +0100

    Swapped out placeholders
    To render actual tweets

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const twitterLib = require('../lib/twitter')
 
-router.get('/', (req,res,next)=>{
-    res.render('index')
+router.get('/', twitterLib.getLastTweets,
+    (req,res,next)=>{
+        res.render('index',{
+            tweets: res.locals.tweets
+        })
+})
+
+router.post('/', twitterLib.tweet,
+    (req,res,next)=>{
+        res.redirect('/')
+})
+
+
+router.get('/schedule', (req,res,next)=>{
+    res.render('schedule');
 })
 
 module.exports = router;
