ece33e38bcd24a22643b2785c185f4140004cd49
Connor Murphy <dev.web@engineer.com>
2018-06-29 01:24:43 +0100

    Added agenda and scheduled tweets

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,59 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const twitterLib = require('../lib/twitter')
+const twitterLib = require("../lib/twitter");
 
-router.get('/', twitterLib.getLastTweets,
-    (req,res,next)=>{
-        res.render('index',{
-            tweets: res.locals.tweets
-        })
-})
+router.get("/", twitterLib.getLastTweets, (req, res, next) => {
+  res.render("index", {
+    tweets: res.locals.tweets
+  });
+});
 
-router.post('/', twitterLib.tweet,
-    (req,res,next)=>{
-        res.redirect('/')
-})
+router.post("/", twitterLib.tweet, (req, res, next) => {
+  res.redirect("/");
+});
 
+router.get("/schedule", (req, res, next) => {
+    req.db.collection('scheduledTweets').find({})
+    .toArray((err,docs)=>{
+        if(err){
+            res.render('error',{
+                message: err
+            })
+        }
+        else{
+            let scheduledTweets = []
+            for(let i = 0; i < docs.length; i++){
+                if(docs[i].nextRunAt){
+                    let tmp = {
+                        'tweet' : docs[i].data.tweet,
+                        'when' : docs[i].nextRunAt.toString().substr(0,21)
+                    }
+                    scheduledTweets.push(tmp)
 
-router.get('/schedule', (req,res,next)=>{
-    res.render('schedule');
-})
+                }
+                else{
+                    console.log('remove ' + docs[i]._id)
+                    console.log(docs[i].data.tweet)
+                }
+            }
+            res.render('schedule', {
+                tweets : scheduledTweets
+            })
+        }
+    });
+});
+
+router.post("/schedule", (req, res, next) => {
+    let when = req.body.date +'T'+ req.body.time;
+    twitterLib.agenda.schedule(
+        when,
+        "tweet",
+        { tweet: req.body.tweetText },
+        () => {
+            console.log("job persisted in db");
+        }
+    );
+    res.redirect('/schedule');
+});
 
 module.exports = router;
